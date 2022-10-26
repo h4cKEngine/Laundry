57912c0838ce08d78220594ac223f4a457e16540
luca <ciao.arena@gmail.com>
2022-10-26 17:21:37 +0200

    h

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -12,14 +12,14 @@ class Reservation extends Model
     public $table='reservations';
 
     public function lavasciugaPrenota(){
-        return $this->belongsTo(Washer::class,  'id_lav', 'id');
+        return $this->belongsTo(Washer::class,  'id_lav', 'id_reservation');
     }
 
     public function prenotazioneProgrLav(){
-        return $this->belongsTo(WashingProgram::class, 'id_progr_lav', 'id');
+        return $this->belongsTo(WashingProgram::class, 'id_progr_lav', 'id_reservation');
     }
 
     public function utente(){
-        return $this->belongsTo(User::class, '',  '', '');
+        return $this->belongsTo(User::class, 'id_utente',  'id_reservation');
     }
 }
