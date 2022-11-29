b61e9f8043c1312af4f6d07e4efc4a22560e94c7
bob <hack_engine@outlook.it>
2022-11-29 21:50:34 +0100

    blade prima stesura

---
+++ resources/views/user.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>User</title>
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" href="css/normalize.css">
+
+    </head>
+    <body class="antialiased">
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -51,9 +51,22 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
+    return view('guest');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
 Route::get('/carbon', function () {
     return view('carbon');
 });
+
+Route::get('/user', function () {
+    return view('user');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
